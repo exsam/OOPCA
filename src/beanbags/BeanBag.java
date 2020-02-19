f5b3123c0d66f9b6f2cb1c257de0b83f44c80643
Sam Higson <sh961@exeter.ac.uk>
2020-02-19 12:18:40 +0000

    Created Basic BeanBag Class
    as part of planning, created basic BeanBag class

---
+++ src/beanbags/BeanBag.java
@@ -0,0 +1,37 @@
+package beanbags;
+
+/**
+ * BeanBags is a class for implementing BeanBag objects
+ *
+ * @author 690027367
+ * @version 0.1
+ */
+
+public class BeanBag {
+    private String name;
+    private String ID;
+    private String manufacture;
+    private String information;
+
+    private int price;
+    private short year;
+    private byte month;
+
+    public BeanBag(String ID, String manufacture, short year, byte month) {
+        this.ID = ID;
+        this.manufacture = manufacture;
+        this.year = year;
+        this.month = month;
+    }
+
+    public BeanBag(String name, String ID, String manufacture, String information, short year, byte month) {
+        this.name = name;
+        this.ID = ID;
+        this.manufacture = manufacture;
+        this.information = information;
+        this.year = year;
+        this.month = month;
+    }
+
+
+}
