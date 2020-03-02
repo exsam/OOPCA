ab550e5bf2bf645d3c642f8c55f16fea22df99cf
Sam Higson <sh961@exeter.ac.uk>
2020-03-02 09:39:51 +0000

    sorted out interface implementation

--- src/TestingClassSam.java
+++ src/TestingClassSam.java
@@ -6,7 +6,7 @@ public class TestingClassSam {
   public static void main(String[] args)
       throws IllegalIDException, BeanBagMismatchException, InvalidMonthException,
           IllegalNumberOfBeanBagsAddedException {
-    Store testStore = new Store();
+    BeanBagStore testStore = new Store();
     BeanBag testBag = new BeanBag("NAME", "Test", "TEST BAG", (short) 2020, (byte) 12);
     testStore.addBeanBags(10, "testing", "Sam", "RUN", (short) 2020, (byte) 12);
     testStore.addBeanBags(3, "test2", "SAM", "BATER", (short) 2020, (byte) 12);
