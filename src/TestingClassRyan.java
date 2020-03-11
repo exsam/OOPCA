575ff1727ffb79e411616eacc0334af077968664
rw600 <rw600@exeter.ac.uk>
2020-03-11 16:15:23 +0000

    Test`

--- src/TestingClassRyan.java
+++ src/TestingClassRyan.java
@@ -10,28 +10,22 @@ public class TestingClassRyan {
       System.out.print("Total Bean Bags in Stock: ");
       System.out.print(testing.beanBagsInStock());
       System.out.print("\n Reserved Bags in Stock: ");
-
       System.out.print(testing.reservedBeanBagsInStock());
-      System.out.print("\n Reserving Bean Bag with ID = 00000004");
+      /*System.out.print("\n Reserving Bean Bag with ID = 00000004");
       System.out.print("\n Reservation number = ");
       System.out.print(testing.reserveBeanBags(1, "00000004"));
       System.out.print("\n Reserved Bags in Stock: ");
-      System.out.print(testing.reservedBeanBagsInStock());
+      System.out.print(testing.reservedBeanBagsInStock());*/
 
-
-      System.out.println("\nUNIQUE: " + testing.getNumberOfDifferentBeanBagsInStock());
-
-      System.out.println(testing.beanBagsInStock());
-
-      System.out.println(Store.stockList.size());
+      /*System.out.println("\nUNIQUE: " + testing.getNumberOfDifferentBeanBagsInStock());
+      System.out.println(testing.beanBagsInStock());*/
     } catch (Exception e) {
       e.printStackTrace();
     }
-
-    /*try {
+    try {
       testing.saveStoreContents("stocklist.txt");
     } catch (Exception e) {
       e.printStackTrace();
-    }*/
+    }
   }
 }
