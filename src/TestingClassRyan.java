461ab884a643ec6ad2a2c516df7673bd8d46a997
rw600 <rw600@exeter.ac.uk>
2020-03-12 17:43:20 +0000

    Added Test data to TestingClassRyan

--- src/TestingClassRyan.java
+++ src/TestingClassRyan.java
@@ -6,34 +6,49 @@ public class TestingClassRyan {
     // System.out.println("Testing Start");
     BeanBagStore testing = new Store();
     try {
+      testing.empty();
+
+      testing.addBeanBags(10,"man","nam","00000000",(short)2010,(byte)12);
+      testing.addBeanBags(10,"manu","nam","00000006",(short)2010,(byte)12);
+      testing.addBeanBags(10,"man","nam","00007000",(short)2010,(byte)12);
+      testing.addBeanBags(4,"man","nam","00000004",(short)2010,(byte)12);
+      testing.setBeanBagPrice("00000004", 50);
+      testing.setBeanBagPrice("00000000", 10);
+
+      try {
+        testing.saveStoreContents("stocklist.ser");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
       testing.loadStoreContents("stocklist.ser");
 
-
-      //System.out.println("Testing ID 00000002");
-      //testing.getBeanBagDetails("00000002");
-
-      //System.out.println("Total Bean Bags in Stock: " + testing.beanBagsInStock());
-      //System.out.println("Reserved Bags in Stock: " + testing.reservedBeanBagsInStock());
-      //System.out.println("*Reserving 3 of Bean Bag with ID = 00000004");
-      //System.out.println("Reservation number = " + testing.reserveBeanBags(3, "00000004"));
-      //System.out.println("--------------------------------------------");
-      //System.out.println("Reserved Bags in Stock: " + testing.reservedBeanBagsInStock());
-
-
-      //System.out.println("UN-Reserving Bean Bag with Reservation No: 3)");
-      //testing.unreserveBeanBags(3);
-      //System.out.print("\n Reserved Bags in Stock: ");
-      //System.out.print(testing.reservedBeanBagsInStock());
-
-      //System.out.println("The total Price of reserved beanbags = " + testing.getTotalPriceOfReservedBeanBags());
-      //System.out.println("The info for bean bag ID 00000002 = " + testing.getBeanBagDetails("00000002"));
-
-      //testing.sellBeanBags(1,"00000005");
-      /*System.out.println("\nUNIQUE: " + testing.getNumberOfDifferentBeanBagsInStock());
-      System.out.println(testing.beanBagsInStock());*/
-
-      //testing.empty();
+      System.out.println("--------------------------------------------");
+      System.out.println("Total Bean Bags in Stock: " + testing.beanBagsInStock());
+      System.out.println("Reserved Bags in Stock: " + testing.reservedBeanBagsInStock());
+      System.out.println("*Reserving 3 of Bean Bag with ID = 00000004");
+      System.out.println("Reservation number = " + testing.reserveBeanBags(3, "00000004"));
+      System.out.println("*Reserving 2 of Bean Bag with ID = 00000000");
+      System.out.println("Reservation number = " + testing.reserveBeanBags(2, "00000000"));
+      System.out.println("--------------------------------------------");
+      System.out.println("Reserved Bags in Stock: " + testing.reservedBeanBagsInStock());
+      System.out.println("The total Price of reserved beanbags = " + testing.getTotalPriceOfReservedBeanBags());
+      System.out.println("--------------------------------------------");
+      System.out.println("UN-Reserving Bean Bag with Reservation No: 1)");
+      testing.unreserveBeanBags(1);
+      System.out.println("--------------------------------------------");
+      System.out.println("Reserved Bags in Stock: " + testing.reservedBeanBagsInStock());
+      System.out.println("The total Price of reserved beanbags = " + testing.getTotalPriceOfReservedBeanBags());
+      System.out.println("The number of Unique Bean Bags in stock = " + testing.getNumberOfDifferentBeanBagsInStock());
+      System.out.println("--------------------------------------------");
+      System.out.println("Emptying the store");
+      System.out.println("--------------------------------------------");
+      testing.empty();
+      System.out.println("Total Bean Bags in Stock: " + testing.beanBagsInStock());
+      System.out.println("Reserved Bags in Stock: " + testing.reservedBeanBagsInStock());
+      System.out.println("The number of Unique Bean Bags in stock = " + testing.getNumberOfDifferentBeanBagsInStock());
+      System.out.println("The total Price of reserved beanbags = " + testing.getTotalPriceOfReservedBeanBags());
       //testing.addBeanBags(10,"man","nam","00000000",(short)2010,(byte)12);
+
     } catch (Exception e) {
       e.printStackTrace();
     }
