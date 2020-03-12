7f156ec4ec0c6aa12fa88e46e1e71a2f695d6d22
rw600 <rw600@exeter.ac.uk>
2020-03-12 12:47:39 +0000

    Commented the Check Class

--- src/TestingClassRyan.java
+++ src/TestingClassRyan.java
@@ -13,19 +13,19 @@ public class TestingClassRyan {
       System.out.print(testing.reservedBeanBagsInStock());
       System.out.print("\n Reserving Bean Bag with ID = 00000004");
       System.out.print("\n Reservation number = ");
-      System.out.print(testing.reserveBeanBags(1, "00000004"));
+      System.out.print(testing.reserveBeanBags(2, "00000004"));
       System.out.print("\n Reserved Bags in Stock: ");
       System.out.print(testing.reservedBeanBagsInStock());
       System.out.print("\n UN-Reserving Bean Bag with ID = 00000004 (Reservation No: 3)");
       testing.unreserveBeanBags(3);
       // NEED TO TEST THIS WITH DIFFERENT QUANTITES VS JUST 1!!!!
-      System.out.print("\n Reserved Bags in Stock: ");
-      System.out.print(testing.reservedBeanBagsInStock());
+      //System.out.print("\n Reserved Bags in Stock: ");
+      //System.out.print(testing.reservedBeanBagsInStock());
       System.out.print("\n The total Price of reserved beanbags = ");
       System.out.print(testing.getTotalPriceOfReservedBeanBags());
       System.out.print("\n The info for bean bag ID 00000002 = ");
       System.out.print(testing.getBeanBagDetails("00000002"));
-      testing.sellBeanBags(1,"00000005");
+      //testing.sellBeanBags(1,"00000005");
       /*System.out.println("\nUNIQUE: " + testing.getNumberOfDifferentBeanBagsInStock());
       System.out.println(testing.beanBagsInStock());*/
     } catch (Exception e) {
--- src/beanbags/Check.java
+++ src/beanbags/Check.java
@@ -16,32 +16,44 @@ public class Check {
    * @param id
    * @throws IllegalIDException
    */
+
+  // This function checks that the passed ID has a range 00000000-7FFFFFFF.
   public static void validID(String id) throws IllegalIDException {
-    // this means ID has a range 00000000-7FFFFFFF
+    // If the passed ID parameter has the correct length of "8".
     if (id.length() == 8) {
       try {
+        // Converts a signed hexadecimal number to an integer named "deci".
         int deci = Long.valueOf(id, 16).intValue();
+        // If the "deci" integer is less than 0.
         if (deci < 0) {
-          throw new IllegalIDException(
-              "ID must be a positive number (within range 00000000-7FFFFFFF)");
+          // Throw "IllegalIDException" as the ID is invalid (Wrong Range).
+          throw new IllegalIDException("ID must be a positive number (within range 00000000-7FFFFFFF)");
         }
       } catch (NumberFormatException e) {
+        // Throw "IllegalIDException" as the ID is invalid (Wrong format).
         throw new IllegalIDException("ID is not a Hexidecimal number.");
       }
     } else {
+      // Throw "IllegalIDException" as the ID is invalid (Wrong Length).
       throw new IllegalIDException("ID must only be 8 characters long.");
     }
   }
 
+  // This function checks if the ID of a passed bag matches the rest of the data for that ID.
   public static void matchingIDs(BeanBag bag)
       throws BeanBagMismatchException {
-    // System.out.println("MatchingID's Was called");
+
+    // Loop through every object in the "stockList" object array list.
     for (int i = 0; i < stockList.size(); i++) {
+      // Create temporary "tempBag" object from current "stockList" location.
       BeanBag tempBag = (BeanBag) stockList.get(i);
+      // Check that if the ID of the "tempBag" matches the ID of the bag in the "stockList".
       if ((bag.getID()).equals(tempBag.getID())) {
+        // If the Name, Manufacter and Infomation accross both bags matches.
         if (bag.getName().equals(tempBag.getName())
             & bag.getName().equals(tempBag.getManufacturer())
             & bag.getInformation().equals(tempBag.getInformation())) {
+          // Throw "BeanBagMismatchException" as the ID doe not match the rest of the data.
           throw new BeanBagMismatchException("ID does not match existing");
         }
       }
@@ -50,28 +62,43 @@ public class Check {
 
   public static void fulfillRequest(int num, String id) throws BeanBagIDNotRecognisedException,
           BeanBagNotInStockException,InsufficientStockException {
+    // Define integer "currentCounter" and set it's value to 0.
     int currentCounter = 0;
+    // Define integer "oldCounter" and set it's value to 0.
     int oldCounter = 0;
+    // Loop through every object in the "stockList" object array list.
     for (int i = 0; i < stockList.size(); i++) {
+      // Define a temporary bag called "bag" from the array location.
       BeanBag bag = (BeanBag) stockList.get(i);
+      // If the Bag ID matches the passed parameter ID AND the bag is NOT reserved and NOT sold.
       if ((bag.getID()).equals(id) && !bag.getReserved() && !bag.isSold()) {
+        // If the bag is sold.
         if (bag.isSold()) {
+          // Increment integer "oldCounter" by 1.
           oldCounter++;
+        // If te bag is not sold.
         } else {
+          // Increment integer "currentCounter" by 1.
           currentCounter++;
         }
-        System.out.print("oldCounter: " + oldCounter);
-        System.out.print("currentCounter: " + currentCounter);
+        //System.out.print("oldCounter: " + oldCounter);
+        //System.out.print("currentCounter: " + currentCounter);
       }
     }
 
+    // If the "currentCounter" and "oldCounter" both = 0.
     if (currentCounter == 0 & oldCounter == 0) {
+      // Throw "BeanBagIDNotRecognisedException", as the ID is not found.
       throw new BeanBagIDNotRecognisedException("No BeanBags with this ID exist on our system.");
     }
+    // If the "currentCounter" = 0 and the "oldCounter" is greater than or equal to 1.
     if (currentCounter == 0 & oldCounter >= 1) {
+      // Throw "BeanBagIDNotInStockException", as the beanBag is not in stock.
       throw new BeanBagNotInStockException(id + " is not in stock.");
     }
+    // If the "currentCounter" is smaller than "num".
     if (currentCounter < num) {
+      // Throw "InsufficientStockException", as there are not enough beanBags in stock for the order.
       throw new InsufficientStockException("We don't have enough BeanBags to fulfil this order.");
     }
   }
